1e3b431a449f187d41a093e6e28b0ef899ac8f99
AshkenSC <393940378@qq.com>
2018-12-28 23:24:50 +0800

    Initializting linear classification sample program

---
+++ linearClassification/linear_classification.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+# create characteristic list
+column_names = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size',
+                'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',
+                'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+
