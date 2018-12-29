5342926889d8f2c34bff6fe8dff5a14fc8cfc8b5
AshkenSC <393940378@qq.com>
2018-12-29 09:53:49 +0800

    linear classification: data import complete

--- linearClassification/linear_classification.py
+++ linearClassification/linear_classification.py
@@ -6,3 +6,15 @@ column_names = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Siz
                 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',
                 'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
 
+# load designated data from Internet
+data = pd.read_csv(
+        'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data',
+        names=column_names)
+
+
+# replace '?' with standard missing value
+data = data.replace(to_replace='? ', value=np.nan)
+# abandon data with missing value (as long as there is one dimension missing)
+data = data.dropna(how='any')
+# output the amount and dimension of 'data'
+data.shape
