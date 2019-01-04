88c3a5aaa85fac489a821813739eb89f310c98da
AshkenSC <393940378@qq.com>
2019-01-04 14:23:42 +0800

    (SVM) print out shapes of the split datasets

--- SVM/handwriting_identification.py
+++ SVM/handwriting_identification.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_digits
 # store digits image data in variable 'digits'
 digits = load_digits()
 # inspect data size and dimension
-digits.data.shape()
+print(digits.data.shape)
 
 # handwriting data division
 from sklearn.cross_validation import train_test_split
@@ -12,5 +12,5 @@ X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target,
                                                     test_size=0.25, random_state=33)
 
 # inspect training and test set respectively
-y_train.shape()
-y_test.shape()
+print(y_train.shape)
+print(y_test.shape)
