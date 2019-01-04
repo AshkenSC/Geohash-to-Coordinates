f70403eea32a4c7a37ad52b0f15ea16768b8fee7
AshkenSC <393940378@qq.com>
2019-01-04 11:21:56 +0800

    (SVM) training and test set divided

---
+++ SVM/handwriting_identification.py
@@ -0,0 +1,12 @@
+# load digit loader from sklearn datasets
+from sklearn.datasets import load_digits
+# store digits image data in variable 'digits'
+digits = load_digits()
+# inspect data size and dimension
+digits.data.shape()
+
+# handwriting data division
+from sklearn.cross_validation import train_test_split
+# randomly select 75% of the dataset as training set, the rest as test set
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target,
+                                                    test_size=0.25, random_state=33)
