f170dd8eeea7f70e49ff377e53c58637ba6a01dc
AshkenSC <393940378@qq.com>
2019-01-04 14:45:24 +0800

    (SVM) train model and make prediction

--- SVM/handwriting_identification.py
+++ SVM/handwriting_identification.py
@@ -8,6 +8,7 @@ print(digits.data.shape)
 # handwriting data division
 from sklearn.cross_validation import train_test_split
 # randomly select 75% of the dataset as training set, the rest as test set
+# random_state: the random seed
 X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target,
                                                     test_size=0.25, random_state=33)
 # inspect training and test set respectively
@@ -19,7 +20,14 @@ from sklearn.preprocessing import StandardScaler
 # load SVM classifier based on linear hypothesis
 from sklearn.svm import LinearSVC
 
-# standardize training and test data
+# normalize training and test data
 ss = StandardScaler()
 X_train = ss.fit_transform(X_train)
 X_test = ss.transform(X_test)
+
+# initialize LinearSVC classifier based on SVM
+lsvc = LinearSVC()
+# train model
+lsvc.fit(X_train, y_train)
+# predict using trained model
+y_predict = lsvc.predict(X_test)
