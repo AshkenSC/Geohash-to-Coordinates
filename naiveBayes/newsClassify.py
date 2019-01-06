f439ccbf213d6d117d78446f2b10d5d15aed6bd8
AshkenSC <393940378@qq.com>
2019-01-06 22:30:28 +0800

    (Naive Bayes) load dataset and split up test set

--- naiveBayes/newsClassify.py
+++ naiveBayes/newsClassify.py
@@ -5,3 +5,9 @@ news = fetch_20newsgroups(subset='all')
 # inspect data scale and details
 print(len(news.data))
 print(news.data[0])
+
+# load train_test_split from sklearn
+from sklearn.cross_validation import train_test_split
+# get test set
+X_train, X_test, y_train, y_test = train_test_split(news.data, news.target,
+                                                    test_size=0.25, random_state=33)
