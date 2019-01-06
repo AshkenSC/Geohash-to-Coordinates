bf6a6d83784ebbc4694b61c32a413f13e5cd7f82
AshkenSC <393940378@qq.com>
2019-01-06 22:24:09 +0800

    (Naive Bayes) inspect data scale and details

--- naiveBayes/newsClassify.py
+++ naiveBayes/newsClassify.py
@@ -2,3 +2,6 @@
 from sklearn.datasets import fetch_20newsgroups
 # download data from Internet
 news = fetch_20newsgroups(subset='all')
+# inspect data scale and details
+print(len(news.data))
+print(news.data[0])
