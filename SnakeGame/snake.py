1a90a4dd09826fe9f8c58e036bccc18def3e052b
AshkenSC <393940378@qq.com>
2019-02-01 23:26:08 +0800

    New functiond drawGrid implemented to draw grid on the game window

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -1,20 +1,37 @@
 import pygame
 
+def redrawWindow(surface):
+    global rows, width
+    surface.fill((0, 0, 0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
+def drawGrid(width, rows, surface):
+    '''绘制游戏背景方格'''
+    sizeBetween = width // rows
+    x = 0
+    y = 0
+
+    for l in range(rows):
+        x += sizeBetween
+        y += sizeBetween
+        # 绘制一条纵向线和横向线
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, width))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (width, y))
+
 def main():
+    global width, rows
     width = 500
-    height = 500
     rows = 20
-    win = pygame.display.set_mode((width, height))
+    win = pygame.display.set_mode((width, width))
     flag = True
 
     clock = pygame.time.Clock()
 
+    # 游戏主循环
     while flag:
         pygame.time.delay(50)
         clock.tick(10)
         redrawWindow(win)
 
-def redrawWindow(win):
-    win.fill((0, 0, 0))
-    drawGrid(surface)
-    pygame.display.update()
+main()
