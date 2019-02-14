72e1dc0ebc024ad29ea789b34919ee69989416bc
AshkenSC <393940378@qq.com>
2019-02-14 22:56:51 +0800

    Using numpy array as data input and display data with different format styles

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
+import numpy as np
 
-plt.plot([1, 2, 3, 4], [2, 4, 8, 16], 'ro')
-plt.axis([0, 6, 0, 20])
+# 每隔200毫秒等时距取样
+t = np.arange(0., 5., 0.2)
+
+# 红色虚线，蓝色方块，绿色三角
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 plt.show()
