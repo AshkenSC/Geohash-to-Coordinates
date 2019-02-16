277c1f2212a6eb31f172eadd915d78b8719ca73d
AshkenSC <393940378@qq.com>
2019-02-16 23:42:33 +0800

    Plotting with different format styles

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -1,13 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = {'a': np.arange(50),
-        'c': np.random.randint(0, 50, 50),
-        'd': np.random.randn(50)}
-data['b'] = data['a'] + 10 * np.random.randn(50)
-data['d'] = np.abs(data['d']) * 100
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
 
-plt.scatter('a', 'b', c='c', s='d', data=data)
-plt.xlabel('entry a')
-plt.ylabel('entry b')
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 plt.show()
