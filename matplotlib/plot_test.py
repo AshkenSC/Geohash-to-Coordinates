a5d82956d847972fb78459d81642ca2253cd7a2f
AshkenSC <393940378@qq.com>
2019-02-17 23:28:44 +0800

    Plotting with keyword strings

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# evenly sampled time at 200ms intervals
-t = np.arange(0., 5., 0.2)
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
 
-# red dashes, blue squares and green triangles
-plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.scatter('a', 'b', c='c', s='d', data=data)
+plt.xlabel('entry a')
+plt.ylabel('entry b')
 plt.show()
