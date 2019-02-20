9c3470ff8402b114bd6563bdef113460135db9cb
AshkenSC <393940378@qq.com>
2019-02-20 23:26:59 +0800

    Calibrating graph

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 6, 9, 16, 25]
-plt.plot(squares, linewidth = 5)
+plt.plot(input_values, squares, linewidth = 5)
 
 # set caption and label axes
 plt.title("Square numbers", fontsize=24)
