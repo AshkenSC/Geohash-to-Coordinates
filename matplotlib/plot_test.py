3bab9924c4beec88cdb18dc365a59e24fc95a69d
AshkenSC <393940378@qq.com>
2019-02-21 23:55:49 +0800

    Use scatter() to generate scattering graph

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -1,16 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 6, 9, 16, 25]
-plt.plot(input_values, squares, linewidth = 5)
-
-# set caption and label axes
-plt.title("Square numbers", fontsize=24)
-plt.xlabel("Value", fontsize=14)
-plt.ylabel("Square of Value", fontsize=14)
-
-# set tick size
-plt.tick_params(axis='both', labelsize=14)
-
+plt.scatter(2, 4)
 plt.show()
