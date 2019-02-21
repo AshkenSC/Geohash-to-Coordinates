87a7080c2bc84b421a1e26f7e8e1fc30a63ffd8a
AshkenSC <393940378@qq.com>
2019-02-21 23:57:35 +0800

    Set caption and add labels for the graph

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -1,5 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+plt.scatter(2, 4, s=200)
+
+# Set caption and add labels
+plt.title("Square Number", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
 plt.show()
