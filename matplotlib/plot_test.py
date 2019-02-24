fa2cc6d204adb897ea9b9373077ef7bfb68619ed
AshkenSC <393940378@qq.com>
2019-02-24 14:56:49 +0800

    Book chapter complete: subplots

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -1,5 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3, 4, 5], [1, 2, 3, 5, 8])
+names = ['sub_a', 'sub_b', 'sub_c']
+values = [5, 25, 125]
+
+plt.figure(num='hello', figsize=(9, 3))
+
+plt.subplot(1,3,2)
+plt.bar(names, values)
+plt.subplot(1,3,1)
+plt.scatter(names, values)
+plt.subplot(1,3,3)
+plt.plot(names, values)
+plt.suptitle('Subplots')
 plt.show()
+
