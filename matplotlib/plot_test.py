9151ba9ddeb1df71f0e2b21ca4e4cc82944d3658
AshkenSC <393940378@qq.com>
2019-02-25 18:53:39 +0800

    Book chapter updated: drawing bar graph

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -3,5 +3,6 @@ import matplotlib.pyplot as plt
 
 data = [2, 4, 8, 16, 32]
 
-plt.bar(range(len(data)), data)
+plt.bar([0.3, 1.7, 4, 6, 7], data, width=0.6, bottom=[10, 0, 5, 0, 5], color='rgbrg')
 plt.show()
+
