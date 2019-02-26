2887211da6265dad241ea7776172669d4a990a54
AshkenSC <393940378@qq.com>
2019-02-26 22:10:34 +0800

    Book chapter update: matplotlib complete

--- matplotlib/plot_test.py
+++ matplotlib/plot_test.py
@@ -4,8 +4,9 @@ labels = 'Andy','Mary','Tom','Susan'
 data = [30,40,45,20]
 
 explode = (0.1,0.2,0.3,0.1)
-plt.pie(data, explode=explode, labels=labels)
+plt.pie(data, explode=explode, labels=labels, autopct='%.2f')
 
+plt.title('pie chart')
 plt.axis('equal')
 plt.show()
 
