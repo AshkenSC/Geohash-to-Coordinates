69a1d19f344b690cb9263b1eea5dd1a6dc6b503a
AshkenSC <393940378@qq.com>
2019-05-14 23:31:58 +0800

    Book chapter update: updating chapter 3.2 "Analyzing stock data"

--- python_book/stock/stock.py
+++ python_book/stock/stock.py
@@ -1,5 +1,5 @@
 import numpy as np
-c, v = np.loadtxt("petro_china.csv", delimiter=',', usecols=(3,5), unpack=True, skiprows=1)
+close, volume = np.loadtxt("petro_china.csv", delimiter=',', usecols=(3,5), unpack=True, skiprows=1)
 # skiprows=1 设置跳过开头的行数（不是行号）
-print(c)
-print(v)
+print(close)
+print(volume)
