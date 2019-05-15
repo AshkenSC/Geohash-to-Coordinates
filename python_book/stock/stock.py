87d1ea73bd16cffa211e41acaabd73549eb9ef74
AshkenSC <393940378@qq.com>
2019-05-15 22:11:00 +0800

    (1) Book source code update: stock.py has been updated;
    (2) Book chapter update: chapter 3.2.1 "Calculating average values" has
    been updated.

--- python_book/stock/stock.py
+++ python_book/stock/stock.py
@@ -1,5 +1,16 @@
 import numpy as np
 close, volume = np.loadtxt("petro_china.csv", delimiter=',', usecols=(3,5), unpack=True, skiprows=1)
 # skiprows=1 设置跳过开头的行数（不是行号）
-print(close)
-print(volume)
+
+# 1. 计算成交量加权平均价
+vwap = np.average(close,weights=volume)
+print('vwap=',vwap)
+
+# 2. 计算收盘时算术平均价
+mean = np.mean(close)
+print('mean=',mean)
+
+# 3. 计算收盘时加权平均价（时间与现在越近，权重越大）
+t = np.arange(len(close))
+twap = np.average(close,weights=t)
+print('twap=',twap)
