c4301f31bb625ff5103f6b157856bc03250f104b
AshkenSC <393940378@qq.com>
2019-05-15 22:43:25 +0800

    (1) Book source code update: stock.py has been updated;
    (2) Book chapter update: chapter 3.2.1 "Calculating average values"
    has been updated.

--- python_book/stock/stock.py
+++ python_book/stock/stock.py
@@ -4,13 +4,31 @@ close, volume = np.loadtxt("petro_china.csv", delimiter=',', usecols=(3,5), unpa
 
 # 1. 计算成交量加权平均价
 vwap = np.average(close,weights=volume)
-print('vwap=',vwap)
+print('成交量加权平均价 vwap=',vwap)
 
 # 2. 计算收盘时算术平均价
 mean = np.mean(close)
-print('mean=',mean)
+print('收盘时算术平均价 mean=',mean)
 
 # 3. 计算收盘时加权平均价（时间与现在越近，权重越大）
 t = np.arange(len(close))
 twap = np.average(close,weights=t)
-print('twap=',twap)
+print('收盘时加权平均价 twap=',twap)
+
+# 4. 获取最高价、最低价
+high,low = np.loadtxt('petro_china.csv', delimiter=',',usecols=(2,4),unpack=True, skiprows=1)
+print('该时段每日最高价：')
+print(high)     # 输出最高价
+print('该时段每日最低价：')
+print(low)      # 输出最低价
+
+highest = np.max(high)
+print('时段内历史最高价 highest=', highest)
+lowest = np.min(low)
+print('时段内历史最低价 lowest=', lowest)
+average = (highest+lowest)/2
+print('均值 average = ',average)
+
+# 5. 计算最大值和最小值的波动范围
+print('最大值波动范围=',np.ptp(high))
+print('最小值波动范围=',np.ptp(low))
