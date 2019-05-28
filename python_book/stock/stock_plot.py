f698743da65a6298cdf99a5c984810fe7b858b6a
AshkenSC <393940378@qq.com>
2019-05-28 21:41:40 +0800

    Book chapter update: updating chapter 3.3.1 "Plotting  SMA"

--- python_book/stock/stock_plot.py
+++ python_book/stock/stock_plot.py
@@ -2,14 +2,14 @@ import numpy as np
 from matplotlib.pyplot import plot
 from matplotlib.pyplot import show
 
-# 简单移动平均线
+# 绠�鍗曠Щ鍔ㄥ钩鍧囩嚎
 
-N =5
+N = 5      # 5澶╃Щ鍔ㄥ钩鍧囩嚎
 weights = np.ones(N) / N
-print('权重 weights =',weights)
+print('鏉冮噸 weights =',weights)
 
 close = np.loadtxt("petro_china.csv", delimiter=',', usecols=(3, ), unpack=True, skiprows=1)
-sma = np.convolve(weights,close)[N-1:-N+1]     # 简单移动平均线
+sma = np.convolve(weights,close)[N-1:-N+1]     # 绠�鍗曠Щ鍔ㄥ钩鍧囩嚎
 t = np.arange(N - 1, len(close))
 plot(t, close[N - 1:], lw=1.0)
 plot(t, sma, lw=2.0)
