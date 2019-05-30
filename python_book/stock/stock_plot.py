e5879916326b722a083577b229a8bf16887a53fc
AshkenSC <393940378@qq.com>
2019-05-30 21:24:35 +0800

    Book source code update: updating source code of chapter 3.3.1 "Plotting SMA"

--- python_book/stock/stock_plot.py
+++ python_book/stock/stock_plot.py
@@ -1,16 +1,21 @@
 import numpy as np
-from matplotlib.pyplot import plot
-from matplotlib.pyplot import show
+import matplotlib.pyplot as plt
+
+# 绘制简单移动平均线
+
+N = 5      # 绘制5天移动平均线，因此N取5
+weights = np.ones(N) / N
+
+# 收盘价
+close = np.loadtxt("petro_china.csv", delimiter=',', usecols=(3, ), unpack=True, skiprows=1)
 
 # 简单移动平均线
+sma = np.convolve(weights,close)[N-1:-N+1]
 
-N = 5      # 5天移动平均线
-weights = np.ones(N) / N
-print('权重 weights =',weights)
-
-close = np.loadtxt("petro_china.csv", delimiter=',', usecols=(3, ), unpack=True, skiprows=1)
-sma = np.convolve(weights,close)[N-1:-N+1]     # 简单移动平均线
+# 横坐标
 t = np.arange(N - 1, len(close))
-plot(t, close[N - 1:], lw=1.0)
-plot(t, sma, lw=2.0)
-show()
+
+plt.plot(t, close[N - 1:], lw=1.0, color='red')  # 绘制收盘价
+plt.plot(t, sma, lw=2.0, color='blue')           # 绘制SMA
+plt.legend(loc='best', labels=['close', 'sma'])  # 绘制图例
+plt.show()
