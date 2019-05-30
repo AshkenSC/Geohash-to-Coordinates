bad7241c750a9010d245fb389d1a8f2c63a0c7d7
AshkenSC <393940378@qq.com>
2019-05-30 22:24:26 +0800

    Book chapter update: updating chapter 3.3.1 "Plotting SMA"

--- python_book/stock/stock_plot.py
+++ python_book/stock/stock_plot.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 绘制简单移动平均线
+'''绘制简单移动平均线'''
 
 N = 5      # 绘制5天移动平均线，因此N取5
 weights = np.ones(N) / N
 
-# 收盘价
+# 从数据源获取收盘价
 close = np.loadtxt("petro_china.csv", delimiter=',', usecols=(3, ), unpack=True, skiprows=1)
 
 # 简单移动平均线
