a9dd0ea341c28a961623649ff7e739e035047061
AshkenSC <393940378@qq.com>
2019-07-12 21:59:30 +0800

    fasttextGensim.py updated. Model has been created. Model test is done.

--- word_vector/fasttext/fasttextGensim.py
+++ word_vector/fasttext/fasttextGensim.py
@@ -4,7 +4,7 @@ from gensim.models import FastText
 
 '''数据集去除多余字符'''
 def remove_char(write_file, read_file):
-    f=codecs.open(write_file, "a+",'utf-8')
+    f=codecs.open(write_file, "w",'utf-8')
 
     i = 0
     for line in open(read_file, encoding='utf-8'):
@@ -18,7 +18,7 @@ def remove_char(write_file, read_file):
 
 
 '''数据集导入与训练'''
-def train(open_file):
+def train(open_file, dim):
     # 将文本行存入列表
     i = 1
     lines = []
@@ -28,10 +28,30 @@ def train(open_file):
         i += 1
     print('Data input complete.')
     # 训练数据
-    model = FastText(lines, size=50, min_count=2, iter=5)
+    model = FastText(lines, size=dim, min_count=3, iter=5)
     model.save('testModel.model')   # 保存为model格式
     model.wv.save_word2vec_format('testModelVec.vector', binary=False) # 保存为vector
 
 
-remove_char(write_file='clean_data.txt', read_file='test.txt')
-train('clean_data.txt')
+'''训练模型（如模型已存在请勿重复加载训练）'''
+#remove_char(write_file='clean_data.txt', read_file='data_train.txt')
+#train('clean_data.txt', 200)
+
+'''模型加载与测试'''
+# 载入模型
+model = FastText.load('testModel.model')
+
+word1 = '劲舞团'
+word2 = '石油'
+word3 = '原油'
+# 获取词向量
+print(word1 + ' 的词向量为：')
+print(model.wv[word1])
+# 求相似度
+print(word1 + ' 和 ' + word2  + ' 的相似度为：')
+print(model.wv.similarity(word1, word2))
+print(word3 + ' 和 ' + word2 + ' 的相似度为：')
+print(model.wv.similarity(word3, word2))
+# 求最相似词语
+print('和' + word1 + ' 最相似的词语为：')
+print(model.most_similar(word1))
