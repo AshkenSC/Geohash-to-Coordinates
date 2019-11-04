a743ee9101549f0422344cb3f6d77cde191ed0e8
AshkenSC <393940378@qq.com>
2019-11-04 23:47:41 +0800

    NLMM: train.py has been updated.

--- NLP/NNLM/train.py
+++ NLP/NNLM/train.py
@@ -6,36 +6,36 @@ https://zhuanlan.zhihu.com/p/37886740
 import numpy as np
 import tensorflow as tf
 
-# 定义文件路径
-TRAIN_DATA = 'ptb.train'
-TEST_DATA = 'ptb.test'
-EVAL_DATA = 'ptb.valid'
+'''定义文件路径与超参数'''
+TRAIN = 'ptb.train'
+TEST = 'ptb.test'
+EVAL = 'ptb.valid'
 
-# 定义超参数
 HIDDEN_SIZE = 300
-NUM_LAYERS = 2  # LSTM 结构的层数
-VOCAB_SIZE = 10000  # 字典规模
-TRAIN_BATCH_SIZE = 20  # 训练数据batch大小
-TRAIN_NUM_STEP = 35  # 训练数据阶段长度
+NUM_LAYERS = 2                  # LSTM结构的层数
+VOCAB_SIZE = 10000              # 字典规模
+TRAIN_BATCH_SIZE = 20           # 训练数据batch大小
+TRAIN_STEP = 35                 # 训练数据阶段长度
 
-EVAL_BATCH_SIZE = 1  # 测试数据batch大小
-EVAL_NUM_STEP = 1  # 测试数据阶段长度
-NUM_EPOCH = 5  # 使用训练数据轮数
+EVAL_BATCH_SIZE = 1             # 测试集batch大小
+EVAL_STEP = 1                   # 测试集阶段长度
+NUM_EPOCH = 5                   # 训练轮次数
 LSTM_KEEP_PROB = 0.9
 EMBEDDING_KEEP_PROB = 0.9
-MAX_GRAD_NORM = 5  # 用于控制梯度膨胀的梯度大小上限
-SHARED_EMB_AND_SOFTMAX = True  # 在softmax层和词向量层之间共享参数
+MAX_GRAD_NORM = 5               # 用于控制梯度膨胀的梯度大小上限
+SHARED_EMB_AND_SOFTMAX = True   # 在softmax层和词向量层之间共享参数
 
 
-# 通过一个PTBModel类来描述模型
+'''使用PTBModel类描述模型'''
 class PTBModel(object):
     def __init__(self, is_training, batch_size, num_steps):
         '''
-        Arg:
-            is_training: 表示是否在训练
-            batch_size: 表示batch size
-            num_stpes: 表示截断长度
+        参数：
+        is_training: 是否在训练
+        batch_size: batch大小
+        num_stpes: 截断长度
         '''
+
         self.batch_size = batch_size
         self.num_steps = num_steps
         self.input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
@@ -52,7 +52,7 @@ class PTBModel(object):
         # 多层结构
         cell = tf.nn.rnn_cell.MultiRNNCell(lstm_cells)
 
-        # 表示初始状态
+        # 初始状态
         self.initial_state = cell.zero_state(batch_size, tf.float32)
 
         # 定义单词的词向量矩阵
@@ -95,7 +95,7 @@ class PTBModel(object):
         self.cost = tf.reduce_sum(loss) / batch_size
         self.final_state = state
 
-        # 如果是训练状态，那么还需要实现反向传播
+        # 如果处于训练状态，还需要进行反向传播
         if not is_training:
             return
 
@@ -108,20 +108,17 @@ class PTBModel(object):
         )
 
 
-# In[4]:
-
-
+'''训练函数'''
 def run_epoch(session, model, batches, train_op, output_log, step):
     '''
-    训练函数
-    Args:
+    参数：
         session 上下文
         model 上述模型的实例
         batches 数据
         train_op
         output_log 是否打印输出日志
         step
-    Return:
+    返回值：
         step
         结果
     '''
@@ -146,103 +143,88 @@ def run_epoch(session, model, batches, train_op, output_log, step):
     return step, np.exp(total_costs / iters)
 
 
-def read_data(file_path):
-    '''
-    读取数据，返回包含单词编号的数组，一整个文本的内容作为一个数组返回，每行句子拼接起来
-    '''
-    with open(file_path, 'r') as fin:
-        id_string = ' '.join([line.strip() for line in fin.readlines()])
-    id_list = [int(w) for w in id_string.split()]
+'''读取文本，将每行句子拼接，一整个文本转化为一个单词序号列表，作为函数返回值'''
+def get_id_list(path):
+    with open(path, 'r') as fin:
+        source_str = ' '.join([line.strip() for line in fin.readlines()])
+    id_list = []
+    for word in source_str.split():
+        id_list.append(int(word))
     return id_list
 
 
-def make_batches(id_list, batch_size, num_step):
+'''获取batch'''
+def get_batch(id_list, batch_size, num_step):
     '''
-    获取到batch
-    Args:
-        id_list: 一整个文本组成的数组，内容是word的id
-        batch_size: batch的大小
-        num_step: 表示训练时的上下文，输入的单词个数
+    参数：
+    id_list: 一整个文本组成的数组，内容是word的id
+    batch_size: batch的大小
+    num_step: 表示训练时的上下文，输入的单词个数
     '''
-    num_batches = (len(id_list) - 1) // (batch_size * num_step)  # batch的数量
-    print
-    num_batches
-    data = np.array(id_list[:num_batches * batch_size * num_step])
-    print
-    data.shape
-    data = np.reshape(data, [batch_size, num_batches * num_step])  # 将数据切分成 batch_size, num_batches * num_steps的数组
-    # 沿着第二个维度将数据切分为num_batches的batch，存入一个数组
-    print
-    data.shape
-    data_batches = np.split(data, num_batches, axis=1)
-    print
-    data_batches[0].shape
 
-    label = np.array(id_list[1:num_batches * batch_size * num_step + 1])
-    label = np.reshape(label, [batch_size, num_batches * num_step])
-    label_batches = np.split(label, num_batches, axis=1)
+    num_batch = (len(id_list) - 1) // (batch_size * num_step)
+    print('batch数量:' + num_batch)
+    data = np.array(id_list[:num_batch * batch_size * num_step])    # 从训练数据中取整
+    print(data.shape)
+    data = np.reshape(data,
+                      [batch_size, num_batch * num_step])           # 将数据切分成 batch_size, num_batch * num_steps的数组 (20, 46445)
+    # (注：尚不完全理解该注释含义）
+    # 沿着第二个维度将数据切分为num_batch的batch，存入一个数组
+    print(data.shape)
+    data_batches = np.split(data, num_batch, axis=1)
+    print(data_batches[0].shape)
+
+    label = np.array(id_list[1:num_batch * batch_size * num_step + 1])
+    label = np.reshape(label, [batch_size, num_batch * num_step])
+    label_batches = np.split(label, num_batch, axis=1)
     return list(zip(data_batches, label_batches))
 
 
-def main():
-    initializer = tf.random_uniform_initializer(-0.05, 0.05)  # 初始化函数
+'''进行训练'''
+initializer = tf.random_uniform_initializer(-0.05, 0.05)  # 初始化函数
 
-    # 训练的RNN模型
-    with tf.variable_scope("language_model", reuse=None, initializer=initializer):
-        train_model = PTBModel(is_training=True, batch_size=TRAIN_BATCH_SIZE, num_steps=TRAIN_NUM_STEP)
+# 训练的RNN模型
+with tf.variable_scope("language_model", reuse=None, initializer=initializer):
+    train_model = PTBModel(is_training=True, batch_size=TRAIN_BATCH_SIZE, num_steps=TRAIN_STEP)
 
-    # 测试用的模型，与train_model共用参数，但没有dropout(is_training=False)
-    with tf.variable_scope("language_model", reuse=True, initializer=initializer):
-        eval_model = PTBModel(is_training=False, batch_size=EVAL_BATCH_SIZE, num_steps=EVAL_NUM_STEP)
+# 测试用的模型，与train_model共用参数，但没有dropout(is_training=False)
+with tf.variable_scope("language_model", reuse=True, initializer=initializer):
+    eval_model = PTBModel(is_training=False, batch_size=EVAL_BATCH_SIZE, num_steps=EVAL_STEP)
 
-    # 训练模型
-    with tf.Session() as sess:
-        # 初始化
-        tf.global_variables_initializer().run()
+# 训练模型
+with tf.Session() as sess:
+    # 初始化
+    tf.global_variables_initializer().run()
 
-        # 训练数据
-        train_batches = make_batches(
-            read_data(TRAIN_DATA),
-            TRAIN_BATCH_SIZE,
-            TRAIN_NUM_STEP
-        )
+    # 训练数据
+    train_batch = get_batch(get_id_list(TRAIN), TRAIN_BATCH_SIZE, TRAIN_STEP)
 
-        # eval数据
-        eval_batches = make_batches(
-            read_data(EVAL_DATA),
-            EVAL_BATCH_SIZE,
-            EVAL_NUM_STEP
-        )
+    # 验证数据
+    eval_batch = get_batch(get_id_list(EVAL), EVAL_BATCH_SIZE, EVAL_STEP)
 
-        # test数据
-        test_batches = make_batches(
-            read_data(TEST_DATA),
-            EVAL_BATCH_SIZE,
-            EVAL_NUM_STEP
-        )
+    # 测试数据
+    test_batch = get_batch(get_id_list(TEST), EVAL_BATCH_SIZE, EVAL_STEP)
 
-        step = 0
-        # 每一轮
-        for i in range(NUM_EPOCH):
-            print("In iteration: %d" % (i + 1))
-            # 训练过程
-            step, train_pplx = run_epoch(sess, train_model, train_batches,
-                                         train_model.train_op,
-                                         output_log=True,
-                                         step=step
-                                         )
-            print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_pplx))
-
-            # evaluation过程
-            _, eval_pplx = run_epoch(sess, eval_model, eval_batches,
-                                     tf.no_op(),
-                                     output_log=False,
-                                     step=0
+    step = 0
+    # 每一轮
+    for i in range(NUM_EPOCH):
+        print("Iteration: %d" % (i + 1))
+        # 训练过程
+        step, train_ppl = run_epoch(sess, train_model, train_batch,
+                                     train_model.train_op,
+                                     output_log=True,
+                                     step=step
                                      )
-            print("Epoch: %d Eval Perplexity: %.3f" % (i + 1, eval_pplx))
-        _, test_pplx = run_epoch(sess, eval_model, eval_batches, tf.no_op(), output_log=False, step=0)
-        # 训练结束进行test
-        print("Test Perplexity: %.3f" % (test_pplx))
+        print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_ppl))
 
+        # 验证过程
+        _, eval_ppl = run_epoch(sess, eval_model, eval_batch,
+                                 tf.no_op(),
+                                 output_log=False,
+                                 step=0
+                                 )
+        print("Epoch: %d Evaluation Perplexity: %.3f" % (i + 1, eval_ppl))
+    _, test_ppl = run_epoch(sess, eval_model, eval_batch, tf.no_op(), output_log=False, step=0)
 
-main()
+    # 训练完成，进行测试
+    print("Test Perplexity: %.3f" % (test_ppl))
