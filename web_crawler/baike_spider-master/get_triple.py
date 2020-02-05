ee6e7c398d6a0118866c07739d56da4220eeda47
AshkenSC <393940378@qq.com>
2020-02-05 21:05:34 +0800

    get_triple.py: 将爬取的词条infobox转为三元组存储，并筛去无关词条。

---
+++ web_crawler/baike_spider-master/get_triple.py
@@ -0,0 +1,57 @@
+import json
+SOURCE = 'baike.json'  # json数据源路径
+DEST = 'baike_triple.txt' # 三元组路径
+
+#keywords = ['病毒', '微生物', '疾病', '传染病', '传染', 'RNA病毒', 'DNA病毒', '冠状病毒', '免疫', '呼吸系统']
+keywords = ['病毒', '微生物', '疾病', '医学', '医学术语', '科学', '科研人员', \
+            '科学百科生命科学分类', '科学百科健康医疗分类', '科学百科农业科学分类']
+
+
+# 读取并筛选json数据
+triples = []
+deleted = []
+cnt = 0
+with open(SOURCE, encoding='utf-8') as f:
+    for json_line in f:  # 对于多行的json必须逐行读取
+        line = json.loads(json_line)
+        # 判断是否是主题相关词条
+        isRelated = False
+        for keyword in keywords:
+            if keyword in line['labels']:
+                isRelated = True
+                break
+        if not line['labels']:  # 针对2019新冠病毒等无标签情况 设置他们为True
+            isRelated = True
+        # 若不相关，则存入被丢弃条目合集
+        if isRelated is False:
+            print('丢弃无关条目：' + line['name'])
+            deleted.append(line)
+            continue
+        # 符合条件且infobox有信息的条目，输出三元组
+        if line['info']:
+            for key, value in line['info'].items():
+                value.replace('\n', ' ')    # 替换value里的换行
+                new_triple = (line['name'], key, value)
+                triples.append(new_triple)
+                # 新增三元组暂时无法print，有utf8编码错误
+                #print('新增三元组：', end='')
+                #print(new_triple)
+                cnt += 1
+    print('读取完毕，共导入三元组' + str(cnt) + '个')
+
+# 写入三元组数据
+output = open(DEST, 'w', encoding='utf-8')
+cnt = 1
+for triple in triples:
+    output.write(triple[0] + ';;;;ll;;;;' + triple[1] + ';;;;ll;;;;' + triple[2] + '\n')
+    #print('正在写入第' + str(cnt) + '个三元组')
+    cnt += 1
+output.close()
+print('三元组写入完毕')
+
+# 写入丢弃条目文件
+output = open('deleted', 'w', encoding='utf-8')
+for item in deleted:
+    output.write(item['name'] + '\n')
+output.close()
+print('废弃条目写入完毕')
