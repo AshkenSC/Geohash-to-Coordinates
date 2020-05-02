8de8d4e7f144f371e4e994b864d01e29dac0270a
AshkenSC <393940378@qq.com>
2020-05-02 10:33:50 +0800

    get_html_from_json.py: 补充了注释并微调了代码结构。

---
+++ web_crawler/baike_spider-master/get_html_from_json.py
@@ -0,0 +1,26 @@
+# 从JSON数据源中提取出每个实体的来源HTML/XML文件
+
+import json
+import os
+
+# JSON数据源路径
+SOURCE = 'classified-merged/classified-merged-json/v2/virus.json'
+# HTML导出文件夹路径
+DEST = r'classified-merged/classified-merged-json/v2/HTML/virus'
+
+def extract_html(source_path, dest_path):
+    source_file = open(source_path, 'r', encoding='utf-8')
+    page_num = 1
+    for json_line in source_file:
+        line = json.loads(json_line)
+        print('正在写入：' + line['name'].encode('gbk', 'ignore').decode('gbk'))
+
+        f = open(os.path.join(dest_path, '{}.htm'.format(page_num)), 'w', encoding='utf-8')
+        f.write(line['html'])
+        f.close()
+
+        print('第{}页面保存完毕'.format(page_num))
+        page_num += 1
+    source_file.close()
+
+extract_html(SOURCE, DEST)
